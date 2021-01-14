cfc6447571ee4aea726b4f7f21f9aeee233da94b
edwinz1 <edwinzacatelco1998@gmail.com>
2021-01-14 08:55:19 -0600

    Materialize funcionando

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 /* eslint-disable no-alert */
 // Script DE FrontEnd
 // Impoertar los Estilos
-import './styles/styles.css';
+// import './styles/styles.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
+
 console.log('Modern JS Working Thanks Babel-...');
 const show = (message = 'Saludo por defecto') => {
   alert(message);
