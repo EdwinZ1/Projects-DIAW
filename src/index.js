96b63e87f68c25045cc8eb1ff22ef01075be7eeb
edwinz1 <edwinzacatelco1998@gmail.com>
2021-01-14 08:35:20 -0600

    Estilos Funcionando

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
 // Script DE FrontEnd
+// Impoertar los Estilos
+import './styles/styles.css';
 console.log('Modern JS Working Thanks Babel-...');
 const show = (message = 'Saludo por defecto') => {
   alert(message);
