931ca3916d89a3fe98fd13f0116f65f0eed417f8
edwinz1 <edwinzacatelco1998@gmail.com>
2021-01-14 07:46:59 -0600

    Prettier y Eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-/* global alert */
-/* eslint-disable no-console */ 
-/* eslint-disable no-alert */ 
-//Script DE FrontEnd
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+// Script DE FrontEnd
 console.log('Modern JS Working Thanks Babel-...');
-const show = (message = 'Saludo por defecto') --> {
-    alert(message);
+const show = (message = 'Saludo por defecto') => {
+  alert(message);
 };
 // EJEMPLO
 show();
-show('Mi saludo')
+show('Mi saludo');
